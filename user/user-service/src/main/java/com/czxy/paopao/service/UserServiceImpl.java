393b959cba0616b6975fafeb0bc6fff285c6feea
18036579096 <971780343@qq.com>
2019-12-05 14:27:01 +0800

    修改了配置文件路径

--- user/user-service/src/main/java/com/czxy/paopao/service/UserServiceImpl.java
+++ user/user-service/src/main/java/com/czxy/paopao/service/UserServiceImpl.java
@@ -2,17 +2,19 @@ package com.czxy.paopao.service;
 
 import com.czxy.paopao.domain.User;
 import org.apache.dubbo.config.annotation.Service;
+
 import javax.annotation.Resource;
+
 import com.czxy.paopao.mapper.UserMapper;
 
 
 @Service(version = "1.0.0")
-public class UserServiceImpl implements UserService{
+public class UserServiceImpl implements UserService {
 
     @Resource
     private UserMapper userMapper;
 
-
+    @Override
     public User findById(Integer id) {
         return userMapper.selectByPrimaryKey(id);
     }
