2f6a438d1430623da1036669dac50dacca396a2a
pink <1805234213@qq.com>
2019-12-08 12:31:09 +0800

    init   order代码

---
+++ commons/commons-dto/src/main/java/com/czxy/paopao/commons/dto/OrderInfo.java
@@ -0,0 +1,13 @@
+package com.czxy.paopao.commons.dto;
+
+import lombok.Data;
+
+/**
+ * Created 路程很长 请别轻易失望 on 2019/12/6.
+ *
+ * @author Keep On Running
+ */
+@Data
+public class OrderInfo {
+    private String xsId;
+}
--- order/order-service/src/main/java/com/czxy/paopao/orderservice/controller/OrderController.java
+++ order/order-service/src/main/java/com/czxy/paopao/orderservice/controller/OrderController.java
@@ -1,7 +1,11 @@
 package com.czxy.paopao.orderservice.controller;
 
+import com.czxy.paopao.commons.dto.OrderInfo;
 import com.czxy.paopao.commons.dto.ResponseResult;
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -15,8 +19,21 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/order")
 public class OrderController {
-    @GetMapping
-    public ResponseResult responseResult(){
-     return new ResponseResult(1,"hha");
+
+    /**
+     * 下单
+     * 说明:点击接单时调用此接口,传入悬赏id,用户id
+     *     必选参数:	xs_id(悬赏id)
+     *     请求类型:	post
+     *     接口地址:	/order/placeAnOrder
+     *     调用例子 : 	/order/placeAnOrder
+     * @return
+     */
+    @PostMapping
+    public ResponseResult responseResult(@RequestBody OrderInfo orderInfo) {
+        // todo 在此处获取 用户 信息    拼接成订单添加到
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        // 获取个人信息
+        return new ResponseResult(1, "hha");
     }
 }
